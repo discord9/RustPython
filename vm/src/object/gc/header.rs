8bd8506667794e0d318e38749e88f5458a559869
discord9 <discord9@163.com>
2022-09-21 23:18:19 +0800

    feat: add cond comp for all ref_cnt

--- vm/src/object/gc/header.rs
+++ vm/src/object/gc/header.rs
@@ -44,6 +44,13 @@ impl GcHeader {
     pub fn inc(&self) -> usize {
         self.ref_cnt.fetch_add(1, Ordering::Relaxed) + 1
     }
+    /// only inc if non-zero(and return true if success)
+    #[inline]
+    pub fn safe_inc(&self) -> bool {
+        self.ref_cnt
+            .fetch_update(Ordering::AcqRel, Ordering::Acquire, |prev| (prev != 0).then(|| prev + 1))
+            .is_ok()
+    }
     /// simple RC -= 1
     pub fn dec(&self) -> usize {
         self.ref_cnt.fetch_sub(1, Ordering::Relaxed) - 1
@@ -53,6 +60,22 @@ impl GcHeader {
     }
     pub fn get(&self) -> usize {
         self.rc()
+    }
+}
+
+impl GcHeader {
+    // move these functions out and give separated type once type range is stabilized
+
+    pub fn leak(&self) {
+        debug_assert!(!self.is_leaked());
+        const BIT_MARKER: usize = (std::isize::MAX as usize) + 1;
+        debug_assert_eq!(BIT_MARKER.count_ones(), 1);
+        debug_assert_eq!(BIT_MARKER.leading_zeros(), 0);
+        self.ref_cnt.fetch_add(BIT_MARKER, Ordering::Relaxed);
+    }
+
+    pub fn is_leaked(&self) -> bool {
+        (self.ref_cnt.load(Ordering::Acquire) as isize) < 0
     }
 }
 
