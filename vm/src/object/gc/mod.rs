edf08a0dee8b4a37de30839594babe7c601e6ec1
discord9 <discord9@163.com>
2022-09-22 13:32:38 +0800

    fix: is_pausing now do not blocking gc thread itself

--- vm/src/object/gc/mod.rs
+++ vm/src/object/gc/mod.rs
@@ -5,5 +5,5 @@ mod collector_sync;
 mod header;
 
 pub use header::GcHeader;
-pub use collector_sync::{CcSync, GLOBAL_COLLECTOR};
+pub use collector_sync::{CcSync, GLOBAL_COLLECTOR, SAME_THREAD_WITH_GC};
 pub use trace::{GcObjPtr, GcTrace, TracerFn, GcStatus};
