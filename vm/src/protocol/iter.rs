e47c7b29526fc0bdb8982eeb8fc18d542c4ec9c2
discord9 <discord9@163.com>
2022-09-24 22:54:43 +0800

    feat trace for `PyBuffer`, `PyIterReturn`, `PyMapping`

--- vm/src/protocol/iter.rs
+++ vm/src/protocol/iter.rs
@@ -156,6 +156,17 @@ pub enum PyIterReturn<T = PyObjectRef> {
     StopIteration(Option<PyObjectRef>),
 }
 
+#[cfg(feature = "gc")]
+impl<T: crate::object::gc::GcTrace> crate::object::gc::GcTrace for PyIterReturn<T> {
+    fn trace(&self, tracer_fn: &mut crate::object::gc::TracerFn) {
+        match self {
+            PyIterReturn::Return(r) => r.trace(tracer_fn),
+            PyIterReturn::StopIteration(Some(obj)) => obj.trace(tracer_fn),
+            _ => (),
+        }
+    }
+}
+
 impl PyIterReturn {
     pub fn from_pyresult(result: PyResult, vm: &VirtualMachine) -> PyResult<Self> {
         match result {
@@ -217,6 +228,16 @@ where
     obj: O, // creating PyIter<O> is zero-cost
     length_hint: Option<usize>,
     _phantom: std::marker::PhantomData<T>,
+}
+
+#[cfg(feature = "gc")]
+impl<'a, T, O> crate::object::gc::GcTrace
+    for PyIterIter<'a, T, O>
+    where O: crate::object::gc::GcTrace + Borrow<PyObject> 
+{
+    fn trace(&self, tracer_fn: &mut crate::object::gc::TracerFn) {
+        self.obj.trace(tracer_fn)
+    }
 }
 
 impl<'a, T, O> PyIterIter<'a, T, O>
