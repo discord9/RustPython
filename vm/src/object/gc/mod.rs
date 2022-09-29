668099024d57e29d340389984a8b397520be1c89
discord9 <discord9@163.com>
2022-09-29 17:07:36 +0800

    doc: more FIXME

--- vm/src/object/gc/mod.rs
+++ vm/src/object/gc/mod.rs
@@ -3,7 +3,7 @@
 mod collector_sync;
 mod header;
 mod trace;
-
+// FIXME(discord9): incorrect way of drop and dealloc cause heap corruprtion and access voialtion
 pub use collector_sync::{CcSync, GLOBAL_COLLECTOR, IS_GC_THREAD};
 pub use header::GcHeader;
 pub use trace::{GcObjPtr, GcStatus, GcTrace, TracerFn};
