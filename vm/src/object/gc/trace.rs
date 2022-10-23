c9d1a841d59caed34839f18d36d4259d28ffd833
Discord9 <discord9@163.com>
2022-10-23 18:52:54 +0800

    feat: add `list_traceable` macro

--- vm/src/object/gc/trace.rs
+++ vm/src/object/gc/trace.rs
@@ -1,3 +1,6 @@
+use std::{any::TypeId, collections::HashSet};
+
+use once_cell::sync::Lazy;
 use rustpython_common::lock::PyRwLock;
 
 use crate::{object::PyObjectPayload, AsObject, PyObject, PyObjectRef, PyRef};
@@ -113,3 +116,111 @@ trace_tuple!((A, 0), (B, 1), (C, 2), (D, 3));
 trace_tuple!((A, 0), (B, 1), (C, 2), (D, 3), (E, 4));
 trace_tuple!((A, 0), (B, 1), (C, 2), (D, 3), (E, 4), (F, 5));
 trace_tuple!((A, 0), (B, 1), (C, 2), (D, 3), (E, 4), (F, 5), (G, 6));
+
+pub struct TraceHelper {}
+
+/// apply a macro to a list of traceable type. using macro instead of generic
+/// because otherwise require specialization feature to enable
+#[macro_export]
+macro_rules! list_traceable {
+    ($MACRO_NAME: tt) => {{
+        use $crate::builtins::*;
+        use $crate::builtins::{
+            enumerate::PyReverseSequenceIterator,
+            function::PyCell,
+            list::{PyListIterator, PyListReverseIterator},
+            memory::{PyMemoryViewIterator, PyMemoryViewNewArgs},
+            super_::PySuperNewArgs,
+            tuple::PyTupleIterator,
+        };
+        use $crate::function::{ArgCallable, ArgIterable, ArgMapping, ArgSequence};
+        use $crate::protocol::{
+            PyBuffer, PyIter, PyIterIter, PyIterReturn, PyMapping, PyNumber, PySequence,
+        };
+        $MACRO_NAME!(
+            // builtin types
+            // PyRange, PyStr is acyclic, therefore no trace needed for them
+            PyBaseException,
+            PyBoundMethod,
+            PyDict,
+            PyEnumerate,
+            PyReverseSequenceIterator,
+            PyFilter,
+            PyFunction,
+            PyBoundMethod,
+            PyCell,
+            IterStatus<PyObjectRef>,
+            PositionIterInternal<PyObjectRef>,
+            PySequenceIterator,
+            PyCallableIterator,
+            PyList,
+            PyListIterator,
+            PyListReverseIterator,
+            PyMap,
+            PyMappingProxy,
+            PyMemoryViewNewArgs,
+            PyMemoryViewIterator,
+            PyProperty,
+            PySet,
+            PySlice,
+            PyStaticMethod,
+            PySuper,
+            PySuperNewArgs,
+            PyTraceback,
+            PyTuple,
+            PyTupleIterator,
+            // FIXME(discord9): deal with static PyType properly
+            PyType,
+            PyUnion,
+            PyWeakProxy,
+            PyZip,
+            PyBaseException,
+            // iter in iter.rs
+            PySequenceIterator,
+            PyCallableIterator,
+            // iter on types
+            // PyList's iter
+            PyListIterator,
+            PyListReverseIterator,
+            // PyTuple's iter
+            PyTupleIterator,
+            // PyEnumerate's iter
+            PyReverseSequenceIterator,
+            // PyMemory's iter
+            PyMemoryViewIterator,
+            // function/Arg protocol
+            ArgCallable,
+            ArgIterable,
+            ArgMapping,
+            ArgSequence,
+            // protocol
+            // struct like
+            PyBuffer,
+            PyIter,
+            PyIterIter<PyObjectRef>,
+            PyIterReturn,
+            PyMapping,
+            PyNumber,
+            PySequence
+        )
+    }};
+}
+
+macro_rules! get_type_ids {
+    ($($TY: ty),*$(,)?) => {
+        [$(
+            std::any::TypeId::of::<$TY>()
+        ),*]
+    };
+}
+pub static TRACEABLE_TYPE: Lazy<HashSet<TypeId>> =
+    Lazy::new(|| HashSet::from(list_traceable!(get_type_ids)));
+impl TraceHelper {
+    /// return true if TypeId's corrsponding type is traceable.
+    ///
+    /// soundness: if extremely rare hash collision happen with TypeId(see [this](https://github.com/rust-lang/rust/issues/10389)),
+    /// the worst results is just mistaken a non-traceable type as traceable, which usually doesn't interference with garbage collection
+    pub fn is_traceable(tid: TypeId) -> bool {
+        TRACEABLE_TYPE.contains(&tid)
+    }
+}
