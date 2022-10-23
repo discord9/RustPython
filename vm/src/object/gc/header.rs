ab12b3dd6002e3a51efdcd236f25291d9c609328
Discord9 <discord9@163.com>
2022-10-23 18:57:07 +0800

    feat: gc interface

---
+++ vm/src/object/gc/header.rs
@@ -0,0 +1,232 @@
+use std::sync::atomic::Ordering;
+
+use crate::object::gc::{Collector, GLOBAL_COLLECTOR};
+use rustpython_common::lock::PyMutexGuard;
+use rustpython_common::{
+    atomic::PyAtomic,
+    lock::{PyMutex, PyRwLockReadGuard},
+    rc::PyRc,
+};
+
+/// Garbage collect header, containing ref count and other info, using repr(C) to stay consistent with PyInner 's repr
+#[repr(C)]
+#[derive(Debug)]
+pub struct GcHeader {
+    ref_cnt: PyAtomic<usize>,
+    color: PyMutex<Color>,
+    buffered: PyMutex<bool>,
+    is_drop: PyMutex<bool>,
+    /// check for soundness
+    is_dealloc: PyMutex<bool>,
+    exclusive: PyMutex<()>,
+    gc: PyRc<Collector>,
+}
+
+impl GcHeader {
+    pub fn new() -> Self {
+        Self {
+            ref_cnt: 1.into(),
+            color: PyMutex::new(Color::Black),
+            buffered: PyMutex::new(false),
+            is_drop: PyMutex::new(false),
+            is_dealloc: PyMutex::new(false),
+            exclusive: PyMutex::new(()),
+            /// when threading, using a global GC
+            #[cfg(feature = "threading")]
+            gc: GLOBAL_COLLECTOR.clone(),
+            /// when not threading, using a gc per thread
+            #[cfg(not(feature = "threading"))]
+            gc: GLOBAL_COLLECTOR.with(|v| v.clone()),
+        }
+    }
+
+    pub fn exclusive(&self) -> PyMutexGuard<()> {
+        self.exclusive.lock()
+    }
+
+    pub fn gc(&self) -> PyRc<Collector> {
+        self.gc.clone()
+    }
+
+    pub fn is_drop(&self) -> bool {
+        *self.is_drop.lock()
+    }
+
+    pub fn set_drop(&self) {
+        *self.is_drop.lock() = true
+    }
+
+    pub fn is_dealloc(&self) -> bool {
+        *self.is_dealloc.lock()
+    }
+
+    pub fn set_dealloc(&self) {
+        *self.is_dealloc.lock() = true
+    }
+
+    pub fn is_cycle(&self) -> bool {
+        self.color() == Color::BlackFree && self.rc() == 0
+    }
+
+    pub(crate) fn check_set_drop_dealloc(&self) -> bool {
+        let is_dealloc = self.is_dealloc();
+        if is_dealloc {
+            warn!("Call a function inside a already deallocated object!");
+            return false;
+        }
+        if !self.is_drop() && !is_dealloc {
+            self.set_drop();
+            self.set_dealloc();
+            true
+        } else {
+            false
+        }
+    }
+
+    /// return true if can drop(also mark object as dropped)
+    pub(crate) fn check_set_drop_only(&self) -> bool {
+        let is_dealloc = self.is_dealloc();
+        if is_dealloc {
+            warn!("Call a function inside a already deallocated object.");
+            return false;
+        }
+        if !self.is_drop() && !is_dealloc {
+            self.set_drop();
+            true
+        } else {
+            false
+        }
+    }
+
+    /// return true if can dealloc(that is already drop)
+    pub(crate) fn check_set_dealloc_only(&self) -> bool {
+        let is_drop = self.is_drop.lock();
+        let is_dealloc = self.is_dealloc();
+        if !*is_drop {
+            warn!("Try to dealloc a object that haven't drop.");
+            return false;
+        }
+        if *is_drop && !is_dealloc {
+            self.set_dealloc();
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn try_pausing(&self) -> Option<PyRwLockReadGuard<()>> {
+        if self.is_dealloc() {
+            warn!("Try to pausing a already deallocated object: {:?}", self);
+            return None;
+        }
+        self.gc.try_pausing()
+    }
+
+    /// This function will block if is a garbage collect is happening
+    pub fn do_pausing(&self) {
+        if self.is_dealloc() {
+            warn!("Try to pausing a already deallocated object: {:?}", self);
+            return;
+        }
+        self.gc.do_pausing();
+    }
+    pub fn color(&self) -> Color {
+        *self.color.lock()
+    }
+    pub fn set_color(&self, new_color: Color) {
+        *self.color.lock() = new_color;
+    }
+    pub fn buffered(&self) -> bool {
+        *self.buffered.lock()
+    }
+    pub fn set_buffered(&self, buffered: bool) {
+        *self.buffered.lock() = buffered;
+    }
+    /// simple RC += 1
+    pub fn inc(&self) -> usize {
+        self.ref_cnt.fetch_add(1, Ordering::Relaxed) + 1
+    }
+    /// only inc if non-zero(and return true if success)
+    #[inline]
+    pub fn safe_inc(&self) -> bool {
+        self.ref_cnt
+            .fetch_update(Ordering::AcqRel, Ordering::Acquire, |prev| {
+                (prev != 0).then_some(prev + 1)
+            })
+            .is_ok()
+    }
+    /// simple RC -= 1
+    pub fn dec(&self) -> usize {
+        self.ref_cnt.fetch_sub(1, Ordering::Relaxed) - 1
+    }
+    pub fn rc(&self) -> usize {
+        self.ref_cnt.load(Ordering::Relaxed)
+    }
+}
+
+impl GcHeader {
+    // move these functions out and give separated type once type range is stabilized
+
+    pub fn leak(&self) {
+        if self.is_leaked() {
+            // warn!("Try to leak a already leaked object!");
+            return;
+        }
+        const BIT_MARKER: usize = (std::isize::MAX as usize) + 1;
+        debug_assert_eq!(BIT_MARKER.count_ones(), 1);
+        debug_assert_eq!(BIT_MARKER.leading_zeros(), 0);
+        self.ref_cnt.fetch_add(BIT_MARKER, Ordering::Relaxed);
+    }
+
+    pub fn is_leaked(&self) -> bool {
+        (self.ref_cnt.load(Ordering::Acquire) as isize) < 0
+    }
+}
+
+/// other color(Green, Red, Orange) in the paper is not in use for now, so remove them in this enum
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    /// In use
+    Black,
+    /// free, as in change by collect_white to black
+    BlackFree,
+    /// Possible member of cycle
+    Gray,
+    /// Member of garbage cycle
+    White,
+    /// Possible root of cycle
+    Purple,
+}
+
+#[derive(Debug, Default)]
+pub struct GcResult {
+    acyclic_cnt: usize,
+    cyclic_cnt: usize,
+}
+
+impl GcResult {
+    fn new(tuple: (usize, usize)) -> Self {
+        Self {
+            acyclic_cnt: tuple.0,
+            cyclic_cnt: tuple.1,
+        }
+    }
+}
+
+impl From<(usize, usize)> for GcResult {
+    fn from(t: (usize, usize)) -> Self {
+        Self::new(t)
+    }
+}
+
+impl From<GcResult> for (usize, usize) {
+    fn from(g: GcResult) -> Self {
+        (g.acyclic_cnt, g.cyclic_cnt)
+    }
+}
+
+impl From<GcResult> for usize {
+    fn from(g: GcResult) -> Self {
+        g.acyclic_cnt + g.cyclic_cnt
+    }
+}
--- vm/src/object/gc/mod.rs
+++ vm/src/object/gc/mod.rs
@@ -1,3 +1,88 @@
+mod collector;
+mod header;
 mod trace;
 
-pub use trace::{Trace, TracerFn};
+pub use collector::{Collector, GLOBAL_COLLECTOR};
+pub use header::{Color, GcHeader, GcResult};
+pub use trace::{Trace, TraceHelper, TracerFn};
+
+pub enum GcStatus {
+    /// should be drop by caller
+    ShouldDrop,
+    /// because object is part of cycle, we don't want double dealloc
+    ShouldDropOnly,
+    /// already buffered, will be dealloc by collector, caller should call `drop_only` to run destructor only but not dealloc memory region
+    BufferedDrop,
+    /// should keep and not drop by caller
+    ShouldKeep,
+    /// Do Nothing, perhaps because it is RAII's deeds
+    DoNothing,
+}
+
+pub fn collect() -> GcResult {
+    #[cfg(feature = "gc")]
+    {
+        #[cfg(feature = "threading")]
+        {
+            GLOBAL_COLLECTOR.force_gc()
+        }
+        #[cfg(not(feature = "threading"))]
+        {
+            GLOBAL_COLLECTOR.with(|v| v.force_gc())
+        }
+    }
+    #[cfg(not(feature = "gc"))]
+    {
+        Default::default()
+    }
+}
+
+pub fn isenabled() -> bool {
+    #[cfg(feature = "gc")]
+    {
+        #[cfg(feature = "threading")]
+        {
+            GLOBAL_COLLECTOR.is_enabled()
+        }
+        #[cfg(not(feature = "threading"))]
+        {
+            GLOBAL_COLLECTOR.with(|v| v.is_enabled())
+        }
+    }
+    #[cfg(not(feature = "gc"))]
+    {
+        false
+    }
+}
+
+pub fn enable() {
+    #[cfg(feature = "gc")]
+    {
+        #[cfg(feature = "threading")]
+        {
+            GLOBAL_COLLECTOR.enable()
+        }
+        #[cfg(not(feature = "threading"))]
+        {
+            GLOBAL_COLLECTOR.with(|v| v.enable())
+        }
+    }
+    #[cfg(not(feature = "gc"))]
+    return;
+}
+
+pub fn disable() {
+    #[cfg(feature = "gc")]
+    {
+        #[cfg(feature = "threading")]
+        {
+            GLOBAL_COLLECTOR.disable()
+        }
+        #[cfg(not(feature = "threading"))]
+        {
+            GLOBAL_COLLECTOR.with(|v| v.disable())
+        }
+    }
+    #[cfg(not(feature = "gc"))]
+    return;
+}
