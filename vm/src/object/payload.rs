e2c508d3f69fb4e526d750807f88fda8d3ecfc59
Discord9 <discord9@163.com>
2022-12-09 00:03:10 +0800

    feat: add `trace` in vtable

--- vm/src/object/payload.rs
+++ vm/src/object/payload.rs
@@ -2,7 +2,7 @@ use super::{Py, PyObject, PyObjectRef, PyRef, PyResult};
 use crate::{
     builtins::{PyBaseExceptionRef, PyType, PyTypeRef},
     types::PyTypeFlags,
-    vm::VirtualMachine
+    vm::VirtualMachine,
 };
 
 cfg_if::cfg_if! {
@@ -20,17 +20,17 @@ cfg_if::cfg_if! {
         use crate::object::MaybeTrace;
         pub trait PyPayload: std::fmt::Debug + PyThreadingConstraint + Sized + MaybeTrace + 'static {
             fn class(vm: &VirtualMachine) -> &'static Py<PyType>;
-        
+
             #[inline]
             fn into_pyobject(self, vm: &VirtualMachine) -> PyObjectRef {
                 self.into_ref(vm).into()
             }
-        
+
             #[inline(always)]
             fn special_retrieve(_vm: &VirtualMachine, _obj: &PyObject) -> Option<PyResult<PyRef<Self>>> {
                 None
             }
-        
+
             #[inline]
             fn _into_ref(self, cls: PyTypeRef, vm: &VirtualMachine) -> PyRef<Self> {
                 let dict = if cls.slots.flags.has_feature(PyTypeFlags::HAS_DICT) {
@@ -40,13 +40,13 @@ cfg_if::cfg_if! {
                 };
                 PyRef::new_ref(self, cls, dict)
             }
-        
+
             #[inline]
             fn into_ref(self, vm: &VirtualMachine) -> PyRef<Self> {
                 let cls = Self::class(vm);
                 self._into_ref(cls.to_owned(), vm)
             }
-        
+
             #[inline]
             fn into_ref_with_type(self, vm: &VirtualMachine, cls: PyTypeRef) -> PyResult<PyRef<Self>> {
                 let exact_class = Self::class(vm);
@@ -70,7 +70,7 @@ cfg_if::cfg_if! {
                 }
             }
         }
-        
+
         pub trait PyObjectPayload:
             std::any::Any + std::fmt::Debug + PyThreadingConstraint + MaybeTrace + 'static
         {
@@ -78,17 +78,17 @@ cfg_if::cfg_if! {
     }else{
         pub trait PyPayload: std::fmt::Debug + PyThreadingConstraint + Sized + 'static {
             fn class(vm: &VirtualMachine) -> &'static Py<PyType>;
-        
+
             #[inline]
             fn into_pyobject(self, vm: &VirtualMachine) -> PyObjectRef {
                 self.into_ref(vm).into()
             }
-        
+
             #[inline(always)]
             fn special_retrieve(_vm: &VirtualMachine, _obj: &PyObject) -> Option<PyResult<PyRef<Self>>> {
                 None
             }
-        
+
             #[inline]
             fn _into_ref(self, cls: PyTypeRef, vm: &VirtualMachine) -> PyRef<Self> {
                 let dict = if cls.slots.flags.has_feature(PyTypeFlags::HAS_DICT) {
@@ -98,13 +98,13 @@ cfg_if::cfg_if! {
                 };
                 PyRef::new_ref(self, cls, dict)
             }
-        
+
             #[inline]
             fn into_ref(self, vm: &VirtualMachine) -> PyRef<Self> {
                 let cls = Self::class(vm);
                 self._into_ref(cls.to_owned(), vm)
             }
-        
+
             #[inline]
             fn into_ref_with_type(self, vm: &VirtualMachine, cls: PyTypeRef) -> PyResult<PyRef<Self>> {
                 let exact_class = Self::class(vm);
@@ -128,7 +128,7 @@ cfg_if::cfg_if! {
                 }
             }
         }
-        
+
         pub trait PyObjectPayload:
             std::any::Any + std::fmt::Debug + PyThreadingConstraint + 'static
         {
